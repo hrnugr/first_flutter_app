22de1b3af515f5d058ff12a748228f4102d25dcd
Harun <ugur.harun@yandex.com>
2021-01-03 15:35:41 +0300

    fix ListView

--- lib/items.dart
+++ lib/items.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Items extends StatelessWidget {
-  List<String> frameworks;
+  final List<String> frameworks;
   Items([this.frameworks = const []]);
+
+  Widget itemBuilder(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset("assets/flutter-logo.png"),
+          Text(frameworks[index])
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: frameworks
-          .map((item) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset("assets/flutter-logo.png"),
-                    Text(item)
-                  ],
-                ),
-              ))
-          .toList(),
-    );
+    return ListView.builder(
+        itemBuilder: itemBuilder, itemCount: frameworks.length);
   }
 }
