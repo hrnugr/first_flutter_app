4aa73811bd03b2c548cf0bba9a3444ff2c763962
Harun <ugur.harun@yandex.com>
2021-01-08 23:54:20 +0300

    fix aligment

--- lib/pages/itemdetail.dart
+++ lib/pages/itemdetail.dart
@@ -8,7 +8,17 @@ class ItemDetail extends StatelessWidget {
         title: Text("Item Detail"),
       ),
       body: Center(
-        child: Text("Item Details..."),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Item Details..."),
+            RaisedButton(
+              child: Text("Back"),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
       ),
     );
   }
