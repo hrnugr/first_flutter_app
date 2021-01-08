b69787bcd8ee3428e3dca5fb128262e3029b5c15
Harun <ugur.harun@yandex.com>
2021-01-09 00:11:08 +0300

    fiz alignment

--- lib/pages/itemdetail.dart
+++ lib/pages/itemdetail.dart
@@ -7,19 +7,27 @@ class ItemDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text("Item Detail"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Item Details..."),
-            RaisedButton(
+      body: //Center(child:
+          Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/flutter-logo.png"),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Item Detail"),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: RaisedButton(
+              color: Theme.of(context).primaryColor,
               child: Text("Back"),
               onPressed: () => Navigator.pop(context),
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
+      //),
     );
   }
 }
