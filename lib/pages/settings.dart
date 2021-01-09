f2819a05d0ba6d6fbadf48b79aaf05eb9ac1f2aa
Harun <ugur.harun@yandex.com>
2021-01-09 22:34:03 +0300

    Tab Bar View

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,35 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/pages/home.dart';
 
+import './edit.dart';
+import './delete.dart';
+
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: [
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text("Settings Page"),
-            ),
-            ListTile(
-              title: Text("Home"),
-              onTap: () => {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => HomePage(),
-                  ),
-                )
-              },
-            )
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: [
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text("Settings Page"),
+              ),
+              ListTile(
+                title: Text("Home"),
+                onTap: () => {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => HomePage(),
+                    ),
+                  )
+                },
+              )
+            ],
+          ),
         ),
+        appBar: AppBar(
+          title: Text("Item List"),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "Edit",
+                icon: Icon(Icons.create),
+              ),
+              Tab(
+                text: "Delete",
+                icon: Icon(Icons.delete),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          EditPage(),
+          DeletePage(),
+        ]),
       ),
-      appBar: AppBar(
-        title: Text("Item List"),
-      ),
-      body: Container(),
     );
   }
 }
