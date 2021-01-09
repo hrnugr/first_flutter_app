3e27e10f4c7143305aba94713ce62d36ed5d0e2a
Harun <ugur.harun@yandex.com>
2021-01-09 14:38:29 +0300

    Side Drawer Continue

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/home.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text("Settings Page"),
+            ),
+            ListTile(
+              title: Text("Home"),
+              onTap: () => {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => HomePage(),
+                  ),
+                )
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
-        title: Text("Settings Page"),
+        title: Text("Item List"),
       ),
-      body: Center(
-        child: Text("Settings....."),
-      ),
+      body: Container(),
     );
   }
 }
