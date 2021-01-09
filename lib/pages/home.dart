b833dce809b1eaad7899827f4397fcb737c69154
Harun <ugur.harun@yandex.com>
2021-01-09 13:16:20 +0300

    SideDrawer & Hamburger

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            AppBar(
+              title: Text("Choose"),
+              automaticallyImplyLeading: false,
+            ),
+            ListTile(
+              title: Text("Choice 1"),
+              onTap: () => {},
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Item List'),
       ),
