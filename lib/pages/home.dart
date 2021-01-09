ab32027955e150a2fe915505d163d98c976c3721
Harun <ugur.harun@yandex.com>
2021-01-09 11:56:13 +0300

    block back button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../itemmanager.dart';
 
 class HomePage extends StatelessWidget {
-  Map<String, String> frameworks;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/pages/itemdetail.dart
+++ lib/pages/itemdetail.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
 
 class ItemDetail extends StatelessWidget {
-  Map<String, String> item;
+  final Map<String, String> item;
   ItemDetail(this.item);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Item Detail"),
-      ),
-      body: //Center(child:
-          Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(item["img"]),
-          Container(
-            padding: EdgeInsets.all(8.0),
-            child: Text(item["title"]),
-          ),
-          Container(
-            padding: EdgeInsets.all(8.0),
-            child: RaisedButton(
-              color: Theme.of(context).primaryColor,
-              child: Text("Delete Item"),
-              onPressed: () => Navigator.pop(context, true),
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Item Detail"),
+        ),
+        body: //Center(child:
+            Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(item["img"]),
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text(item["title"]),
             ),
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                child: Text("Delete Item"),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            ),
+          ],
+        ),
+        //),
       ),
-      //),
     );
   }
 }
